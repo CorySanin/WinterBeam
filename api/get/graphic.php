c637eb2277423ce31a17ec9dd86dfd69de9dd21b
Benjamin Gordon <BenjaminGordon@twc.com>
2016-09-23 09:08:32 -0400

    Update graphic.php
    Changed:
    
        if($_GET['type'] == background)
    
    to:
    
        if(strpos($_SERVER['REQUEST_URI'],'background') == true)
    
    because it wasn't getting the type from $_GET for some reason. This new line has it look in the URL for the word 'background' and if it sees it it assumes it's a background and if it doesn't it assumes it's an avatar. This can probably be fixed in .htaccess later on but it is a good fix for the time being.

--- api/get/graphic.php
+++ api/get/graphic.php
@@ -5,7 +5,7 @@ include $root . "/includes/include.php";
 
 if(isset($_GET['user']) && $_GET['user'] != ''){
     $member = $useriface->getuser($_GET['user']);
-    if($_GET['type'] == background)
+    if(strpos($_SERVER['REQUEST_URI'],'background') == true)
         $imagelocation = $root . '/uploads/backgrounds/' . $member['background'];
     else
         $imagelocation = $root . '/uploads/avatars/' . $member['avatar'];
